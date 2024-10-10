be5b8e0f7cba374de88300be06feb0f5e16722cb
PeterRanario <peterranario11@gmail.com>
2024-10-11 02:21:56 +0800

    Added DataLogger Class

---
+++ Event Button Logger/applet-3.js
@@ -0,0 +1,9 @@
+class DataLogger {
+    constructor(buttonId, cardContainerId, clearButtonId, logCountId) {
+        this.logButton = document.getElementById(buttonId);
+        this.cardContainer = document.getElementById(cardContainerId);
+        this.clearButton = document.getElementById(clearButtonId);
+        this.logCountElement = document.getElementById(logCountId); 
+        this.loggedData = [];
+    }
+}

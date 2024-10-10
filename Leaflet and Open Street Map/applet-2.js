069eaeb1b3875d0621df2fc4eeeecc406f171bd1
PeterRanario <peterranario11@gmail.com>
2024-10-11 01:19:47 +0800

    Changed the max zoom from 19 to 18

--- Leaflet and Open Street Map/applet-2.js
+++ Leaflet and Open Street Map/applet-2.js
@@ -7,7 +7,7 @@ class LeafletMap {
 
     initTileLayer() {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
+            maxZoom: 18,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.map);
     }

e0b6a2d61b447c6a92c3b9fc135998edbe5358e4
PeterRanario <peterranario11@gmail.com>
2024-11-06 13:49:35 +0800

    added new method

--- applet-4/applet4.js
+++ applet-4/applet4.js
@@ -41,4 +41,15 @@ class Studentlist {
 
         this.renderStudentList(this.students, studentSearchListContainer);
     }
+
+    filterStudents(query, searchListContainer) {
+        const filteredStudents = this.students.filter(student => {
+            const fullName = `${student.student_name} ${student.student_program}`;
+            return fullName.toLowerCase().includes(query.toLowerCase());
+        });
+
+        searchListContainer.innerHTML = '';
+
+        this.renderStudentList(filteredStudents, searchListContainer);
+    }
 }
